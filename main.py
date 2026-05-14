a66cb8ea68621c35b62dbc233a25d567bb3a81e4
yahyaibr <157219841+yahyaibr@users.noreply.github.com>
2026-05-14 01:18:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -65,5 +65,5 @@ def start_auth():
         return jsonify({"error": "Internal server error during request", "message": str(e)}), 500
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
